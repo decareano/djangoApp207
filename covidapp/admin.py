9f6005911f4e6094dbb0634e8968ba3a0a335417
Macadmin Local <macadminlocal@m-w80164tfagx.gateway.sonic.net>
2020-05-11 10:07:17 -0700

    changing settings to accomodate import_export feature

--- covidapp/admin.py
+++ covidapp/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Hospital
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+class HospitalResource(resources.ModelResource):
+    class Meta:
+        model = Hospital
+
+class HospitalAdmin(ImportExportModelAdmin):
+    resource_class = HospitalResource
 
 # Register your models here.
-admin.site.register(Hospital)
+admin.site.register(Hospital, HospitalAdmin)
 
